aed0db9cc303aa3458c1b4af79398beb64fd8ed8
Enes Varol <43502998+tubarvex@users.noreply.github.com>
2021-06-29 22:49:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+#first_name = "Enes"
+#last_name = "Varol"
+#full_name = first_name+""+ last_name
+#print("Hello "+full_name)
+#print(type(name))
+
+#age = 21
+#age += 2
+#print(type(age))
+#print("your age is: "+str(age))
+#print(type(age))
+
+#height = 250.5
+#print(height)
+
+#human = False
+#print(human)
+
+# name = "enes asdasd "
+# #print(len(name))
+# #print(name.find("eb"))
+# #print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("e"))
+# print(name.replace("e","a"))
+# print(name*3)
+#phrase = "Elephant"
+#print(phrase.upper().isupper())
+#a = 2+2
+#print(a)
+#import calendar
+#year = 2021
+#mounth = 1
+#print(calendar.month(year, mounth))
+
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+z = int(input("Now, enter number lower than 5:"))
+if z < 5:
+    if z==1:
+        print(x+y)
+    elif z==2:
+        print(x-y)
+    elif z==3:
+        print(x*y)
+    elif z==4:
+        print(x/y)
+else:
+    print("Enter number lower than 5.")
+
+
+
+
+
